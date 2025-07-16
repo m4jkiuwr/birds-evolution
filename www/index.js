fab55378821b5963aadace12635c1e05a62ddf38
LKRenjoyer <m.ovczarz@gmail.com>
2025-07-16 18:18:32 +0200

    Add, triangle drawing !

--- www/index.js
+++ www/index.js
@@ -1,5 +1,18 @@
 import * as sim from "lib-simulation-wasm";
 
+const drawTriangle = (ctxt, x, y, size) => {
+  ctxt.beginPath();
+  ctxt.moveTo(x, y);
+  ctxt.lineTo(x + size, y + size);
+  ctxt.lineTo(x - size, y + size);
+  ctxt.lineTo(x, y);
+
+  ctxt.fillStyle = 'rgb(0,0,0)';
+  ctxt.fill();
+}
+
+
+
 
 
 const simulation = new sim.Simulation();
@@ -9,18 +22,20 @@ const viewport = document.getElementById('viewport');
 const viewportWidth = viewport.width;
 const viewportHeight = viewport.height;
 
+const viewportScale = window.devicePixelRatio || 1;
+viewport.width = viewportWidth * viewportScale;
+viewport.height = viewportHeight * viewportScale;
+
+viewport.style.width = viewportWidth + 'px';
+viewport.style.height = viewportHeight + 'px';
+
 const ctxt = viewport.getContext("2d");
+ctxt.scale(viewportScale, viewportScale);
+
 ctxt.fillStyle = 'rgb(0,0,0)';
 
 const getCoords = (animal) => {
   return [viewportWidth * animal.x, viewportHeight * animal.y, 10, 10]
 }
 
-
-
-for (const animal of world.animals) {
-
-
-  ctxt.fillRect(...getCoords(animal));
-
-}
+drawTriangle(ctxt, 50, 0, 50);
