bfeb53b22a893c7a098a0346f567b14d920aee5d
LKRenjoyer <m.ovczarz@gmail.com>
2025-07-16 16:19:57 +0200

    Add, World, Animal, Food

--- libs/simulation/src/lib.rs
+++ libs/simulation/src/lib.rs
@@ -1,4 +1,5 @@
 mod world_props;
+use rand::{Rng, RngCore};
 use world_props::*;
 
 #[derive(Debug)]
@@ -7,7 +8,35 @@ pub struct World {
     foods: Vec<Food>,
 }
 
+impl World {
+    fn random(rng: &mut dyn RngCore) -> Self {
+        let animals = (0..40).map(|_| Animal::random(rng)).collect();
+        let foods = (0..60).map(|_| Food::random(rng)).collect();
+        Self { animals, foods }
+    }
+    pub fn animals(&self) -> &Vec<Animal> {
+        &self.animals
+    }
+    pub fn foods(&self) -> &Vec<Food> {
+        &self.foods
+    }
+}
+
 #[derive(Debug)]
 pub struct Simulation {
     world: World,
+}
+
+impl Simulation {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            world: World::random(rng),
+        }
+    }
+}
+
+impl Simulation {
+    pub fn world(&self) -> &World {
+        &self.world
+    }
 }
--- libs/simulation/src/world_props.rs
+++ libs/simulation/src/world_props.rs
@@ -1,4 +1,5 @@
 use nalgebra as na;
+use rand::{Rng, RngCore};
 
 #[derive(Debug)]
 pub struct Animal {
@@ -6,8 +7,30 @@ pub struct Animal {
     rotation: na::Rotation2<f32>,
     speed: f32,
 }
+impl Animal {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            position: na::Point2::new(rng.random(), rng.random()),
+            rotation: na::Rotation2::new(rng.random()),
+            speed: 0.002,
+        }
+    }
+    pub fn position(&self) -> na::Point2<f32> {
+        self.position
+    }
+}
 
 #[derive(Debug)]
 pub struct Food {
     position: na::Point2<f32>,
+}
+impl Food {
+    pub fn random(rng: &mut dyn RngCore) -> Self {
+        Self {
+            position: na::Point2::new(rng.random(), rng.random()),
+        }
+    }
+    pub fn position(&self) -> na::Point2<f32> {
+        self.position
+    }
 }
