a2bfe02bc1de8667c13206e19e47a379d5058c7d
LKRenjoyer <m.ovczarz@gmail.com>
2025-07-19 15:43:41 +0200

    Add, exported all types from simulation

--- src/wasm.ts
+++ src/wasm.ts
@@ -2,4 +2,4 @@ import init, * as wasm from "../libs/simulation-wasm/pkg"
 
 
 export const wasmReady = init()
-export const { Simulation, test_fun } = wasm 
+export const { Simulation, World, Animal, Food, test_fun } = wasm 
